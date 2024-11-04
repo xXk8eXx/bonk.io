e3f7dfad295bf9d422f08a62460ec9624a303e61
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-11-03 17:28:15 -0700

    Delete bonk.io directory

--- bonk.io/js/get_site_data.js
+++
@@ -1,8 +0,0 @@
-{
-  "stage_message_limit" : 1,
-  "site_id" : 10912,
-  "public_campaign_type_priority" : [ 1, 6, 2 ],
-  "multi_campaign_enabled" : true,
-  "stage_campaign_type_priority" : [ 2, 1 ],
-  "public_message_limit" : 1
-}

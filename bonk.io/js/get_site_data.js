4f42441afa904a9cb8b85c75a37ee30e85f059d7
jman1593 <95952762+jman1593@users.noreply.github.com>
2024-11-03 17:22:44 -0700

    upload bonk.io

---
+++ bonk.io/js/get_site_data.js
@@ -0,0 +1,8 @@
+{
+  "stage_message_limit" : 1,
+  "site_id" : 10912,
+  "public_campaign_type_priority" : [ 1, 6, 2 ],
+  "multi_campaign_enabled" : true,
+  "stage_campaign_type_priority" : [ 2, 1 ],
+  "public_message_limit" : 1
+}
